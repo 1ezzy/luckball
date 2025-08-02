975a8f692ac49167ecf7c0ecde2ac6c9eb153e56
1ezzy <ezra.furry@gmail.com>
2025-08-01 22:20:49 -0400

    trying a different path for wrangler config in createRequestHandler

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,17 @@
 import createRequestHandler from '@sveltejs/adapter-cloudflare';
+import type { ExecutionContext, ScheduledEvent } from '@cloudflare/workers-types';
 
 const handler = createRequestHandler({
-	config: '/wrangler.jsonc'
+	config: 'wrangler.jsonc'
 });
 
 export default {
-	async scheduled(event, env: any, ctx) {
+	async scheduled(event: ScheduledEvent, env: any, ctx: ExecutionContext) {
 		await handleScheduledTasks(event, env, ctx);
 	}
 };
 
-async function handleScheduledTasks(event, env: any, ctx) {
+async function handleScheduledTasks(event: ScheduledEvent, env: any, ctx: ExecutionContext) {
 	// test cron job
 	if (event.cron === '40 21 * * 5') {
 		await testCron(env);
