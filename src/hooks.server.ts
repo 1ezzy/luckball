8df49aba6c603968cc2e7f04491fdd2c72e3a5a1
1ezzy <ezra.furry@gmail.com>
2025-08-01 22:23:59 -0400

    getting rid of a package

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,16 @@
 import createRequestHandler from '@sveltejs/adapter-cloudflare';
-import type { ExecutionContext, ScheduledEvent } from '@cloudflare/workers-types';
 
 const handler = createRequestHandler({
 	config: 'wrangler.jsonc'
 });
 
 export default {
-	async scheduled(event: ScheduledEvent, env: any, ctx: ExecutionContext) {
-		await handleScheduledTasks(event, env, ctx);
+	async scheduled(event, env, ctx) {
+		await handleScheduledTasks(event, env);
 	}
 };
 
-async function handleScheduledTasks(event: ScheduledEvent, env: any, ctx: ExecutionContext) {
+async function handleScheduledTasks(event, env) {
 	// test cron job
 	if (event.cron === '40 21 * * 5') {
 		await testCron(env);
