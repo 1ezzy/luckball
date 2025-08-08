9c10db5b3e2cd74cb7aa179cba0c1d6be5e94bde
1ezzy <ezra.furry@gmail.com>
2025-08-08 19:56:04 -0400

    trying to isolate the issue

--- packages/game-logic/src/api/espn-api.ts
+++ packages/game-logic/src/api/espn-api.ts
@@ -11,27 +11,23 @@ export class EspnApiClient {
 	}
 
 	async getWeekEvents(seasonType: string, weekNumber: number): Promise<any> {
-		const baseUrl = 'https://sports.core.api.espn.com/v2/sports/football/leagues/nfl';
-		const response = await fetch(
-			`${baseUrl}/seasons/2025/types/${seasonType}/weeks/${weekNumber}/events`
-		);
-		const data = await response.json();
-
-		const eventsResponse = data.items.map((event: { $ref: string }) => fetch(event.$ref));
-		const eventData = await Promise.all(eventsResponse);
-
-		const eventJsonPromises = eventData.map((res: any) => res.json());
-		const events = await Promise.all(eventJsonPromises);
-
-		const teams = events.flatMap((event) => {
-			const nameParts = event.shortName.split(' ');
-			return [nameParts[0], nameParts[2]];
-		});
-
-		return {
-			events: events || [],
-			teams: teams
-		};
+		// const baseUrl = 'https://sports.core.api.espn.com/v2/sports/football/leagues/nfl';
+		// const response = await fetch(
+		// 	`${baseUrl}/seasons/2025/types/${seasonType}/weeks/${weekNumber}/events`
+		// );
+		// const data = await response.json();
+		// const eventsResponse = data.items.map((event: { $ref: string }) => fetch(event.$ref));
+		// const eventData = await Promise.all(eventsResponse);
+		// const eventJsonPromises = eventData.map((res: any) => res.json());
+		// const events = await Promise.all(eventJsonPromises);
+		// const teams = events.flatMap((event) => {
+		// 	const nameParts = event.shortName.split(' ');
+		// 	return [nameParts[0], nameParts[2]];
+		// });
+		// return {
+		// 	events: events || [],
+		// 	teams: teams
+		// };
 	}
 }
 
