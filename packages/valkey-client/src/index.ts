4ba272af17ad652a14bc2ee640263f504ec790de
1ezzy <ezra.furry@gmail.com>
2025-08-17 14:17:32 -0400

    updating valkey client creation

--- packages/valkey-client/src/index.ts
+++ packages/valkey-client/src/index.ts
@@ -1,6 +1,10 @@
 import Valkey from 'iovalkey';
 
 export function createValkeyClient(url: string) {
+	if (process.env.NODE_ENV === 'build' || process.env.SKIP_VALKEY === 'true') {
+		return null;
+	}
+
 	if (!url) {
 		throw new Error('Valkey URL must be provided.');
 	}
