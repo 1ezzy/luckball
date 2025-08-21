d0e92b1207ac3f85cbe33ed593e4c06003977f03
1ezzy <ezra.furry@gmail.com>
2025-08-20 21:27:52 -0400

    updating cron jobs again

--- apps/cron-jobs/src/index.ts
+++ apps/cron-jobs/src/index.ts
@@ -1,4 +1,4 @@
-import { beginWeek, endWeek, startActiveWeek } from '@luckball/game-logic';
+import { beginWeek, endWeek, startActiveWeek, updateScores } from '@luckball/game-logic';
 import { createValkeyClient } from '@luckball/valkey-client';
 import { createDrizzleClient } from '@luckball/drizzle-client';
 import cron from 'node-cron';
@@ -29,14 +29,19 @@ export class CronJobRunner {
 	}
 
 	async updateScores() {
-		console.log('UPDATING SCORES!');
+		const result = await updateScores(this.valkey);
+		return result.success ? { success: true } : { success: false, error: result.message };
 	}
 }
 
 const runner = new CronJobRunner(process.env);
 
-cron.schedule('0 2 * * 3', () => runner.handleBeginWeek()); // Every Monday
-cron.schedule('0 17 * * 4', () => runner.handleStartActiveWeek()); // Monday noon
-cron.schedule('0 2 * * 2', () => runner.handleEndWeek()); // Tuesday Morning
+// cron jobs for game state
+cron.schedule('0 2 * * 3', () => runner.handleBeginWeek()); // Every Wednesday 2:00
+cron.schedule('0 17 * * 4', () => runner.handleStartActiveWeek()); // Thursday 17:00
+cron.schedule('0 2 * * 2', () => runner.handleEndWeek()); // Tuesday 2:00
 
-cron.schedule('*/1 * * * *', () => runner.updateScores()); // update scores regularly
+// cron jobs to update schedules
+cron.schedule('*/20 19-23 * * 4', () => runner.updateScores()); // Thursday 19:00–23:59
+cron.schedule('*/20 * * * 0', () => runner.updateScores()); // Sunday 00:00–23:59
+cron.schedule('*/20 19-23 * * 1', () => runner.updateScores()); // Monday 19:00-23:59
