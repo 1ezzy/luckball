e9cdd19c486a4f391ee4cbf837276c9bec062db6
1ezzy <ezra.furry@gmail.com>
2026-06-10 16:54:17 -0400

    basic pass at starting to add admin dashboard tools

--- apps/admin-dashboard/src/routes/+page.server.ts
+++ apps/admin-dashboard/src/routes/+page.server.ts
@@ -1,6 +1,9 @@
 import { auth } from '$lib/auth/auth';
-import { redirect, type ServerLoadEvent } from '@sveltejs/kit';
+import { fail, redirect, type Actions, type ServerLoadEvent } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { beginWeek, endWeek, startActiveWeek, updateScores } from '@luckball/game-logic';
+import { valkey } from '$lib/clients/valkey-client';
+import { drizzle } from '$lib/clients/drizzle-client';
 
 export const load: PageServerLoad = async ({ request }: ServerLoadEvent) => {
 	const session = await auth.api.getSession({
@@ -9,5 +12,44 @@ export const load: PageServerLoad = async ({ request }: ServerLoadEvent) => {
 
 	if (!session) {
 		throw redirect(307, '/login');
+	}
+};
+
+export const actions: Actions = {
+	beginWeek: async () => {
+		const result = await beginWeek(valkey);
+
+		if (!result.success) {
+			return fail(400, { error: result.message });
+		}
+
+		return { success: true };
+	},
+	startActiveWeek: async () => {
+		const result = await startActiveWeek(valkey);
+
+		if (!result.success) {
+			return fail(400, { error: result.message });
+		}
+
+		return { success: true };
+	},
+	endWeek: async () => {
+		const result = await endWeek(valkey, drizzle);
+
+		if (!result.success) {
+			return fail(400, { error: result.message });
+		}
+
+		return { success: true };
+	},
+	updateScores: async () => {
+		const result = await updateScores(valkey);
+
+		if (!result.success) {
+			return fail(400, { error: result.message });
+		}
+
+		return { success: true };
 	}
 };
--- apps/admin-dashboard/src/routes/+page.svelte
+++ apps/admin-dashboard/src/routes/+page.svelte
@@ -1,5 +1,31 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { Snippet } from 'svelte';
+</script>
+
+{#snippet actionBtn(label: string)}
+	<button class="btn" type="submit">{label}</button>
+{/snippet}
+
+{#snippet actionCard(btn: Snippet)}
+	<div></div>
+{/snippet}
+
 <div
 	class="flex h-full w-full flex-1 items-center justify-center bg-[url('src/lib/assets/topo.svg')]"
 >
-	<span>Dashboard</span>
+	<div class="flex h-full w-full flex-col items-center justify-center gap-8">
+		<form method="post" action="?/beginWeek" use:enhance>
+			{@render actionBtn('Begin Week')}
+		</form>
+		<form method="post" action="?/startActiveWeek" use:enhance>
+			{@render actionBtn('Start Active Week')}
+		</form>
+		<form method="post" action="?/endWeek" use:enhance>
+			{@render actionBtn('End Week')}
+		</form>
+		<form method="post" action="?/updateScores" use:enhance>
+			{@render actionBtn('Update Matchup Scores')}
+		</form>
+	</div>
 </div>
