629e48c6bd296750dfafff247606da34a395de1e
1ezzy <ezra.furry@gmail.com>
2026-06-11 16:46:03 -0400

    force admin-dashboard to use explicit port, spruced up admin dashboard

--- apps/admin-dashboard/src/routes/+page.svelte
+++ apps/admin-dashboard/src/routes/+page.svelte
@@ -1,31 +1,43 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import type { Snippet } from 'svelte';
 </script>
 
 {#snippet actionBtn(label: string)}
 	<button class="btn" type="submit">{label}</button>
 {/snippet}
 
-{#snippet actionCard(btn: Snippet)}
-	<div></div>
+{#snippet actionDescription(description: string)}
+	<span class="text-primary-content text-center">{description}</span>
+{/snippet}
+
+{#snippet actionCard(btnLabel: string, description: string)}
+	<form
+		class="bg-surface-300 flex w-full flex-col items-center justify-center gap-4 rounded-xl border-2 p-8"
+		method="post"
+		action="?/beginWeek"
+		use:enhance
+	>
+		{@render actionBtn(btnLabel)}
+		{@render actionDescription(description)}
+	</form>
 {/snippet}
 
 <div
-	class="flex h-full w-full flex-1 items-center justify-center bg-[url('src/lib/assets/topo.svg')]"
+	class="flex h-full w-full flex-1 items-center justify-center bg-[url('src/lib/assets/dash-bg.png')] bg-cover"
 >
-	<div class="flex h-full w-full flex-col items-center justify-center gap-8">
-		<form method="post" action="?/beginWeek" use:enhance>
-			{@render actionBtn('Begin Week')}
-		</form>
-		<form method="post" action="?/startActiveWeek" use:enhance>
-			{@render actionBtn('Start Active Week')}
-		</form>
-		<form method="post" action="?/endWeek" use:enhance>
-			{@render actionBtn('End Week')}
-		</form>
-		<form method="post" action="?/updateScores" use:enhance>
-			{@render actionBtn('Update Matchup Scores')}
-		</form>
+	<div class="flex h-full w-1/5 flex-col items-center justify-center gap-8">
+		{@render actionCard('Begin Week', 'Moves the game to the "pending" status')}
+		{@render actionCard(
+			'Start Active Week',
+			'Moves the game to the "in_progress" status and seeds the Valkey database with matchups'
+		)}
+		{@render actionCard(
+			'Update Matchup Scores',
+			'Updates the match scores with mocked data for the "in_progress" game phase'
+		)}
+		{@render actionCard(
+			'End Week',
+			'Moves the game to the "ended" status, calculates winners, updates user records'
+		)}
 	</div>
 </div>
